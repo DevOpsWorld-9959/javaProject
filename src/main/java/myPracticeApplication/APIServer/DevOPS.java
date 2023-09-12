e65656d1293d9251484bb8ff9a3bf8a3b75c3fbb
DevOpsWorld-9959 <kirankumarb871@gmail.com>
2023-09-12 13:59:51 +0530

    Java application

---
+++ src/main/java/myPracticeApplication/APIServer/DevOPS.java
@@ -0,0 +1,13 @@
+package myPracticeApplication.APIServer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevOPS {
+	
+    @GetMapping("/devops")
+    public String welcomeMessage() {
+    	return "Hello Mulasoft technologies";
+    }
+}
---
+++ src/test/java/myPracticeApplication/APIServer/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package myPracticeApplication.APIServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
