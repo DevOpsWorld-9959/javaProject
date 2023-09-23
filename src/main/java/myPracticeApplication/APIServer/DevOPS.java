e6f20adf7345b7b0ead4aadf95e7690ea4768e91
DevOpsWorld-9959 <144777083+DevOpsWorld-9959@users.noreply.github.com>
2023-09-23 09:16:13 +0530

    Update DevOPS.java

--- src/main/java/myPracticeApplication/APIServer/DevOPS.java
+++ src/main/java/myPracticeApplication/APIServer/DevOPS.java
@@ -8,10 +8,10 @@ public class DevOPS {
 	
     @GetMapping("/devops")
     public String welcomeMessage() {
-    	return "Hello Mulasofttechnologies  this is tesing getting tools";
+    	return "Hello Mulasofttechnologies  this is tesing getting tools  this is new tesgin code get code" ;
     }
     @GetMapping("/devops/1")
     public String welcomeMessages() {
-    	return "Hello Mulasofttechnologies  this is tesing getting tools";
+    	return "Hello Mulasofttechnologies  this is tesing getting tools tis is tesicjselkrae skdjflksdjflksdflksdf";
     }	
 }
